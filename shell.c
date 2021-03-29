96e9308dc836da65be429e9987cfa36ec6b89917
ssaucedo208 <46984431+ssaucedo208@users.noreply.github.com>
2021-03-28 17:34:29 -0700

    Create shell.c

---
+++ shell.c
@@ -0,0 +1,228 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+
+int find_pipe(char** cargs);
+
+char** parse(char* s, char* cargs[]) {
+  static char* words[500];
+  memset(words, 0, sizeof(words));
+  
+  char break_chars[] = " \t";
+
+  int i = 0;
+  char* p = strtok(s, break_chars);
+
+  char** redirect = malloc(2 * sizeof(char*));
+  for(int i = 0; i < 2; ++i)
+  {
+    redirect[i] = malloc(BUFSIZ * sizeof(char));
+    redirect[0] = "";
+    redirect[1] = "";
+  }
+  words[i++] = p;
+  while (p != NULL) {
+    p = strtok(NULL, break_chars);
+    if(p == NULL)
+    {
+      break;
+    }
+    if(!strncmp(p, ">", 1))
+    {
+      p = strtok(NULL, break_chars);
+      redirect[0] = "o";
+      redirect[1] = p;
+
+      return redirect;
+    }
+    else if(!strncmp(p, "<", 1))
+    {
+      p = strtok(NULL, break_chars);
+      redirect[0] = "i";
+      redirect[1] = p;
+      return redirect;
+
+    }
+    else if(!strncmp(p, "|", 1))
+    {
+      redirect[0] = "p";
+    }
+    words[i++] = p;
+  }
+  return words;
+}
+
+
+int main(int argc, const char * argv[]) 
+{  
+  char input[BUFSIZ];
+  char last_command[BUFSIZ];
+  char* cargs[BUFSIZ];
+  char* rhs[BUFSIZ];
+  char* lhs[BUFSIZ];
+  int pipedescriptor[2];
+
+  memset(input, 0, BUFSIZ * sizeof(char));
+  memset(last_command, 0, BUFSIZ * sizeof(char));
+  memset(lhs, 0, BUFSIZ * sizeof(char));
+  memset(rhs, 0, BUFSIZ * sizeof(char));
+  bool finished = false;
+  
+  while (!finished) {
+    printf("osh> ");
+    fflush(stdout);
+    bool hold = true;
+    char* waitoffset = strstr(input, "&");
+      if(waitoffset != NULL)
+      {
+        *waitoffset =  ' ';
+        hold = false;
+      }
+    if ((fgets(input, BUFSIZ, stdin)) == NULL) {   // or gets(input, BUFSIZ);
+      fprintf(stderr, "no command entered\n");
+      exit(1);
+    }
+    input[strlen(input) - 1] = '\0';          // wipe out newline at end of string
+    printf("input was: \n'%s'\n", input);
+
+    // check for history (!!) command
+    if (strncmp(input, "!!", 2) == 0) 
+    {
+      if (strlen(last_command) == 0) 
+      {
+      fprintf(stderr, "no last command to execute\n");
+      }
+      printf("last command was: %s\n", last_command);
+      
+    }
+    
+     else if (strncmp(input, "exit()", 6) == 0) {   // only compare first 4 letters
+      finished = true;
+    } else {
+      strcpy(last_command, input);
+      printf("You entered: %s\n", input);   // you will call fork/exec
+      
+      pid_t pid = fork();
+
+      if(pid < 0)
+      {
+          fprintf(stderr, "fork failed\n");
+          exit(1);
+      }
+     else if(pid == 0) 
+      {
+        printf("I am the child. \n");
+        memset(cargs, 0, BUFSIZ * sizeof(char));
+        int hist = 0;
+        char** redirect = parse((hist ? last_command : input), cargs);
+        int history = 0;
+            // if we use '!!' we want to read from MRU cache
+        if(!strncmp(input, "!!", 2)) hist = 1;
+        if(!strncmp(redirect[0], "o", 1))
+        {
+          printf("output saved to ./%s\n", redirect[1]);
+          int fd = open(redirect[1], O_TRUNC | O_CREAT | O_RDWR);
+          dup2(fd, STDOUT_FILENO);
+        }
+        else if(!strncmp(redirect[0], "i", 1))
+        {
+          printf("reading from file: ./%s\n", redirect[1]);
+          int fd = open(redirect[1], O_RDONLY);
+          memset(input, 0, BUFSIZ * sizeof(char));
+          read(fd, input, BUFSIZ * sizeof(char));
+          memset(cargs, 0, BUFSIZ * sizeof(char));
+          input[strlen(input) - 1] = '\0';
+          parse(input, cargs);
+        }
+        else if(!strncmp(redirect[0], "p", 1))
+        {
+          pid_t pidchild;
+          int piperhs = find_pipe(cargs);
+          cargs[piperhs] = "\0";
+          int fail = pipe(pipedescriptor);
+          if(fail < 0)
+          {
+            fprintf(stderr, "pipe creation failed.\n");
+            return 1;
+          }
+          for(int i = 0; i < BUFSIZ; ++i)
+          {
+            lhs[i] = cargs[i];
+          }
+
+          for(int i = 0; i < BUFSIZ; ++i)
+          {
+            int index = i + piperhs + 1;
+            if(cargs[index] == 0)
+            break;
+            rhs[i] = cargs[index];
+          }
+
+          pidchild = fork();
+          if(pidchild < 0)
+          {
+            fprintf(stderr, "fork failed.\n");
+            return 1;
+          }
+
+          if(pidchild != 0)
+          {
+            dup2(pipedescriptor[1], STDOUT_FILENO);
+            close(pipedescriptor[1]);
+            execvp(lhs[0], lhs);
+            exit(0);
+          }
+
+          else
+          {
+            dup2(pipedescriptor[0], STDIN_FILENO);
+            close(pipedescriptor[0]);
+            execvp(rhs[0], rhs);
+            exit(0);
+          }
+          wait(NULL);
+        }
+
+        execvp(input, (char *[]){input, NULL});
+        execvp(cargs[0], cargs);
+        exit(0);
+      }
+      
+     else
+      {
+          printf("I am the parent, the child is %d.\n", pid);
+          if(hold)
+          {
+          wait(NULL);
+          wait(NULL);
+          }
+      }
+    }
+  }
+  printf("osh exited\n");
+  printf("program finished\n");
+  
+  return 0;
+  }
+
+
+
+  int find_pipe(char** cargs)
+  {
+    int index = 0;
+    while(cargs[index] != NULL)
+    {
+        if(!strncmp(cargs[index], "|", 1))
+        {
+            return index;
+        }
+        ++index;
+    }
+    return -1;
+  }
